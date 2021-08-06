2a0b6214e896c59187a18423ee4742bfed681502
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2021-08-06 16:01:40 +0530

    New Enhancements

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Menu, shell, dialog } = require("electron");
+const path = require("path");
+const config = require("./package.json");
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: "100%",
@@ -26,14 +27,118 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
+  // createMenu();
+  const menuItemps = [
+    {
+      label: "Main",
+      submenu: [
+        {
+          label: "Open UMS",
+          click: function openUMS() {
+            // mainWindow.loadURL("https://lpu.live/ums/");
+            shell.openExternal("https://ums.lpu.in/lpuums");
+          },
+        },
+        {
+          label: "Open My Class",
+          click: function openMyClass() {
+            shell.openExternal("https://myclass.lpu.in");
+          },
+        },
+      ],
+    },
+    {
+      label: "Forward",
+      accelerator: "CmdOrCtrl+]",
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          const wc = focusedWindow.webContents;
+          if (wc && wc.canGoForward()) wc.goForward();
+        }
+      },
+    },
+    {
+      label: "Back",
+      accelerator: "CmdOrCtrl+[",
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          const wc = focusedWindow.webContents;
+          if (wc && wc.canGoBack()) wc.goBack();
+        }
+      },
+    },
+    {
+      label: "Help",
+      submenu: [
+        {
+          label: "About",
+          click: function about() {
+            const options = {
+              title: "About",
+              message: "An Desktop App build of LPU Live Chat",
+              detail: `LPU Live Chat - v${config.version}`,
+              icon: "./icons/logo.png",
+            };
+            dialog.showMessageBox(null, options, (response) => {
+              if (response === "Visit Source Code") {
+                shell.openExternal(
+                  "https://github.com/bravo68web/lpu-live-chat"
+                );
+              }
+            });
+          },
+        },
+        {
+          label: "Developer by Bravo68web",
+          click: function madeBy() {
+            shell.openExternal("https://github.com/bravo68web");
+          },
+        },
+        {
+          label: "Found a Bug !!",
+          click: function openIssue() {
+            shell.openExternal(
+              "https://github.com/BRAVO68WEB/lpu-live-chat/issues/new"
+            );
+          },
+        },
+        {
+          label: "Check for Update ..",
+          click: function openIssue() {
+            shell.openExternal(
+              "https://github.com/BRAVO68WEB/lpu-live-chat/releases"
+            );
+          },
+        },
+      ],
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "selectall" },
+      ],
+    },
+    {
+      label: "Exit",
+      role: "quit",
+    },
+  ];
+  const menuStrip = Menu.buildFromTemplate(menuItemps);
+  Menu.setApplicationMenu(menuStrip);
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
